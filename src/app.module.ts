d30edaacbec98df33ec6db19eb040397998cbcb4
nej1044 <nej1044@naver.com>
2023-05-08 11:49:11 +0900

    인터페이스

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { ApiController } from './api/api.controller';
+import { UsersController } from './users/users.controller';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [UsersModule],
+  controllers: [ApiController, UsersController],
+  providers: [],
 })
 export class AppModule {}
