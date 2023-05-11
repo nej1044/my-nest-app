055d752ab431fd3045975edd71e46645b47d4fff
nej1044 <nej1044@naver.com>
2023-05-11 18:11:18 +0900

    Feat: 프로바이더

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,15 +1,36 @@
-import { Body, Controller, Post, Query } from '@nestjs/common';
+import { Body, Controller, Param, Post, Query } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { VerifyEmailDto } from './dto/verify-email.dto';
 import { UsersService } from './users.service';
+import { UserLoginDto } from './dto/user-login.dto';
+import { UserInfo } from './UserInfo';
 
 @Controller('users')
 export class UsersController {
-  constructor(private userService: UsersService) {} // UsersService를 컨트롤러에 주입
+  constructor(private usersService: UsersService) {} // UsersService를 컨트롤러에 주입
 
   @Post()
   async createUser(@Body() dto: CreateUserDto): Promise<void> {
-    const { name, eamil, password } = dto;
+    const { name, email, password } = dto;
     await this.usersService.createUser(name, email, password);
+  }
+
+  @Post()
+  async verifyEmail(@Query() dto: VerifyEmailDto): Promise<string> {
+    const { signupVerifyToken } = dto;
+
+    return await this.usersService.verifyEmail(signupVerifyToken);
+  }
+
+  @Post('/login')
+  async login(@Body() dto: UserLoginDto): Promise<string> {
+    const { email, password } = dto;
+
+    return await this.usersService.login(email, password);
+  }
+
+  @Post('/:id')
+  async getUserInfo(@Param('id') userId: string): Promise<UserInfo> {
+    return await this.usersService.getUserInfo(userId);
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { EmailService } from 'src/email/email.service';
 import * as uuid from 'uuid';
+import { UserInfo } from './UserInfo';
 
 @Injectable()
 export class UsersService {
@@ -18,6 +19,30 @@ export class UsersService {
     마련합니다. 토큰을 만들 때는 유효 기간을 설정하여 일정 기간 동안만 인증이 가능하도록 할 수도 있습니다.*/
     await this.saveUser(name, email, password, signupVerifyToken);
     await this.sendMemberJoinEmail(email, signupVerifyToken); // 회원 가입 인증 메일을 발송합니다.
+  }
+
+  async verifyEmail(signupVerifyToken: string): Promise<string> {
+    // TODO
+    // 1. DB에서 signupVerifyToken으로 회원 가입 처리 중인 유저가 있는지 조회하고 없다면 에러 처리
+    // 2. 바로 로그인 상태가 되도록 JWT를 발급
+
+    throw new Error('Method not implement');
+  }
+
+  async login(email: string, password: string): Promise<string> {
+    // TODO
+    // 1. email, password를 가진 유저가 존재하는지 DB에서 확인하고 없다면 에러 처리
+    // 2. JWT 발급
+
+    throw new Error('Method not implemented');
+  }
+
+  async getUserInfo(userId: string): Promise<UserInfo> {
+    // TODO
+    // 1. userId를 가진 유저가 존재하는지 DB에서 확인하고 없다면 에러 처리
+    // 2. 조회된 데이터를 UserInfo 타입으로 응답
+
+    throw new Error('Method not implemented');
   }
 
   private checkUserExists(email: string) {
